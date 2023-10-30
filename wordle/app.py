8d488938560628ea9fc9c0f66fe5279ba8c684ec
Donny van Eijk <donnyvaneijk873@gmail.com>
2023-10-30 13:40:31 +0100

    Wordle attempts update

--- wordle/app.py
+++ wordle/app.py
@@ -1,7 +1,16 @@
 secret_word = "potato"
 guess = ""
 attempts = 0
-while guess != secret_word:
-    guess = input("Enter guess: ")
+max_attempts = 10
 
-print("You have won!")
+
+while guess != secret_word and attempts < max_attempts:
+    current_attempts = max_attempts - attempts - 1
+    guess = input("Enter guess: ")
+    attempts += 1
+    print("You have " + str(current_attempts) + " left!" )
+
+if guess == secret_word:
+    print("You have won!")
+else:
+    print("Out of attempts. The secret word was:", secret_word)
